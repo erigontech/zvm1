31654cc8df48beb7dd5afc3193a27eb78631138f
Paweł Bylica <pawel@ethereum.org>
2024-10-25 14:53:56 +0200

    host: Inspect the code's account

--- test/state/host.cpp
+++ test/state/host.cpp
@@ -434,6 +434,10 @@ evmc::Result Host::execute_message(const evmc_message& msg) noexcept
     if ((msg.flags & EVMC_DELEGATED) == 0 && is_precompile(m_rev, msg.code_address))
         return call_precompile(m_rev, msg);
 
+    const auto code_acc = m_state.find(msg.code_address);
+    if (code_acc == nullptr || code_acc->code_hash == Account::EMPTY_CODE_HASH)
+        return evmc::Result{EVMC_SUCCESS, msg.gas};
+
     // TODO: get_code() performs the account lookup. Add a way to get an account with code?
     const auto code = m_state.get_code(msg.code_address);
     if (code.empty())
